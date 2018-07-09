4d1dd6f8ce646c215f98a9cd1f5c76cafe7e8de6
root <root@vps547504.ovh.net>
2018-07-09 19:11:46 +0200

    added server

---
+++ server/server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+path = require('path'),
+    app = express();
+
+app.set('port', 80);
+
+//tell express that we want to use the www folder
+//for our static assets
+app.use(express.static(path.join(__dirname, '../app/dist')));
+console.log(__dirname, '../app/dist');
+// Listen for requests
+var server = app.listen(app.get('port'), function () {
+    console.log('The server is running on http://localhost:' + app.get('port'));
+});
